f9d5c5ce0b2032e42593730be17fc59f446a0655
benkarola304-cloud <benkarola304@gmail.com>
2025-11-28 01:32:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,23 @@
-const CACHE_NAME = 'sloth-flash-v4-cn'; // 升级版本号
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
+const CACHE_NAME = 'sloth-v5-final';
+const FILES = [
+    './', './index.html', './manifest.json',
     'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
     'https://unpkg.com/html5-qrcode',
-    'https://cdn-icons-png.flaticon.com/512/3750/3750019.png'
+    'https://cdn-icons-png.flaticon.com/512/2353/2353678.png'
 ];
 
 self.addEventListener('install', e => {
     self.skipWaiting();
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if (key !== CACHE_NAME) return caches.delete(key);
-            })
-        ))
-    );
+    e.waitUntil(caches.keys().then(keys => Promise.all(
+        keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    )));
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
-});
+self.addEventListener('fetch', e => e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+));
