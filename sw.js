325c75a5cb3d02c92db6a52d29b511209f5ff988
benkarola304-cloud <benkarola304@gmail.com>
2025-11-28 04:33:01 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sloth-v21-robust';
+const CACHE_NAME = 'sloth-v22-contrast';
 const FILES = [
     './', './index.html', './manifest.json', './sloth.png',
     'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
